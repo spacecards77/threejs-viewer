75b53f41498217a88c9f61d353daf976bb007f80
dmaiseyenka <dmitriymois@gmail.com>
2025-12-03 20:04:13 +0300

    Расчёт центра конструкции при её создании

--- src/entities/Geometry.ts
+++ src/entities/Geometry.ts
@@ -1,32 +1,40 @@
 import * as THREE from 'three';
-import { Member } from './Member';
-import { Node } from './Node';
+import {Vector3} from 'three';
+import {Member} from './Member';
+import {Node} from './Node';
 import type {IGeometry} from './IGeometry';
 
 export interface GeometryJSON {
-  Members: any[]; // MemberJSON[] but accept any to be flexible
-  Nodes: any[]; // NodeJSON[]
+    //ARCHITECTURE: use type
+    Members: any[];
+    Nodes: any[];
 }
 
 export class Geometry implements IGeometry {
-  public readonly idToNode: Map<number, Node>;
-  public readonly members: Member[];
-  public readonly nodes: Node[];
+    public readonly idToNode: Map<number, Node>;
+    public readonly members: Member[];
+    public readonly nodes: Node[];
+    private center!: Vector3;
 
-  constructor(members: Member[], nodes: Node[]) {
-    this.members = members;
-    this.nodes = nodes;
-    this.idToNode = new Map<number, Node>(nodes.map(n => [n.id, n]));
-  }
+    constructor(members: Member[], nodes: Node[]) {
+        this.members = members;
+        this.nodes = nodes;
+        this.idToNode = new Map<number, Node>(nodes.map(n => [n.id, n]));
 
-  static fromJSON(json: GeometryJSON): Geometry {
-    const nodes = (json.Nodes || []).map(n => Node.fromJSON(n));
-    const members = (json.Members || []).map(m => Member.fromJSON(m));
-    return new Geometry(members, nodes);
-  }
+        this.calculateCenter();
+    }
 
-    getCenter(): THREE.Vector3 {
-        //OPTIMIZE: сделать кэширование результата? сделать за один проход?
+    static fromJSON(json: GeometryJSON): Geometry {
+        const nodes = (json.Nodes || []).map(n => Node.fromJSON(n));
+        const members = (json.Members || []).map(m => Member.fromJSON(m));
+        return new Geometry(members, nodes);
+    }
+
+    public getCenter(): THREE.Vector3 {
+        return this.center;
+    }
+
+    private calculateCenter() {
         const minX = Math.min(...this.nodes.map(n => n.x));
         const maxX = Math.max(...this.nodes.map(n => n.x));
         const minY = Math.min(...this.nodes.map(n => n.y));
@@ -34,7 +42,7 @@ export class Geometry implements IGeometry {
         const minZ = Math.min(...this.nodes.map(n => n.z));
         const maxZ = Math.max(...this.nodes.map(n => n.z));
 
-        return new THREE.Vector3(
+        this.center = new THREE.Vector3(
             (minX + maxX) / 2,
             (minY + maxY) / 2,
             (minZ + maxZ) / 2,
